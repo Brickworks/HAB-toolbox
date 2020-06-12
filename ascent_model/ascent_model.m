5190b4cd556634e84fdb8b68725227f9fb09c2d5
philiplinden <lindenphilipj@gmail.com>
2020-06-12 04:08:19 -0700

    basic ascent rate and altitude over time

--- ascent_model/ascent_model.m
+++ ascent_model/ascent_model.m
@@ -23,7 +23,7 @@ addpath('../balloon_model'); % import balloon configuration files
 clear; % clear workspace variables
 
 % Import balloon parameters
-balloon_name = 'HAB-800';
+balloon_name = 'HAB-1200';
 balloon_parameters = import_balloon(balloon_name);
 
 lift_gas = balloon_parameters.spec.lifting_gas;
@@ -33,6 +33,7 @@ balloon_burst_diameter = balloon_parameters.spec.diameter_burst.value; % [m] Mis
 balloon_burst_volume = (4/3) * pi() * (balloon_burst_diameter / 2)^3; % [m^3]
 balloon_release_volume = balloon_parameters.spec.volume_release.value; % [m^3]
 M = molar_mass(lift_gas); % [kg/mol] molar mass of lifting gas
+Cd = balloon_parameters.spec.drag_coefficient; % coefficient of drag
 
 %% Defining the system of equations
 % First compute the ideal gas properties of the balloon filled with gas as
@@ -40,6 +41,7 @@ M = molar_mass(lift_gas); % [kg/mol] molar mass of lifting gas
 % temperature and pressure as the ambient air.
 V_gas = @(altitude, m_gas) (V(atmo_temp(altitude), atmo_pres(altitude), m_gas, M));
 rho_gas = @(altitude, m_gas) (rho(atmo_temp(altitude), atmo_pres(altitude), M));
+A_balloon = @(altitude, m_gas) ((V_gas(altitude, m_gas) / (4/3 * pi())).^(1/3));
 
 % Next we sum the forces acting on the balloon-payload system.
 F_buoyancy = @(altitude, m_gas) (V_gas(altitude, m_gas) .* (rho_gas(altitude, m_gas) - atmo_density(altitude)) .* -g(altitude));
@@ -54,7 +56,7 @@ F_net = @(altitude, m_gas, m_balloon, m_payload) (F_buoyancy(altitude, m_gas) + 
 %   m_payload     mass of the payload - CONTROL INPUT
 %   m_gas         mass of lifting gas in balloon - CONTROL INPUT
 altitude = 0:35000; % [m]
-m_payload = 2; % [kg] initial mass of the payload
+m_payload = 1.5; % [kg] initial mass of the payload
 m_gas = get_recommended_launch_lifting_mass(altitude(1), balloon_release_volume, M); % [kg] initial lifting mass added to balloon
 
 [T_a, P_a, rho_a] = atmo_model(altitude);
@@ -64,6 +66,7 @@ pressure_burst_altitude = altitude(index_closest_altitude_to_burst_pressure);
 balloon_volume = V_gas(altitude, m_gas);
 [~,index_closest_altitude_to_burst_volume] = min(abs(balloon_burst_volume - balloon_volume));
 volume_burst_altitude = altitude(index_closest_altitude_to_burst_volume);
+
 
 figure(1); clf;
 subplot(2,1,1);
@@ -95,7 +98,39 @@ xlabel('Altitude'); ylabel('Volume (m^3)');
 hold off;
 legend();
 
-%% Next steps - Model ascent rate
-% * Drag force, ascent rate, and other parameters as functions of time
-% * Open system, balloon and payload lose mass over time
-% * Control loop with bleeding ballast/lift gas as control inputs
+%% Ascent rate, drag force, and other parameters as functions of time
+F_drag = @(altitude, ascent_rate) (Cd/2 .* rho_gas(altitude, m_gas) .* -(ascent_rate .^2) .* A_balloon(altitude, m_gas));
+
+dt = 0.1; % [s] time step
+t = 0:dt:3600; % [s] elapsed time since launch
+altitude_0 = 0; % [m] initial altitude
+ascent_rate_0 = 0; % [m/s] initial velocity
+
+eq_of_motion = @(t,x) [x(2); (F_buoyancy(x(1), m_gas) + F_drag(x(1), x(2)) + Fg_balloon(x(1), m_balloon) + Fg_payload(x(1), m_payload)) ./ (m_balloon + m_payload + m_gas)];
+[t, x] = ode45(eq_of_motion, t, [altitude_0, ascent_rate_0]);
+altitude = x(:,1);
+ascent_rate = x(:,2);
+ascent_accel = (F_buoyancy(altitude, m_gas) + F_drag(altitude, ascent_rate) + Fg_balloon(altitude, m_balloon) + Fg_payload(altitude, m_payload)) ./ (m_balloon + m_payload + m_gas);
+
+[~,index_closest_altitude_to_burst_volume] = min(abs(volume_burst_altitude - altitude));
+[~,index_closest_altitude_to_burst_pressure] = min(abs(pressure_burst_altitude - altitude));
+burst_index = min(index_closest_altitude_to_burst_volume, index_closest_altitude_to_burst_pressure);
+
+figure(2); clf;
+subplot(3,1,1); hold on;  grid on;
+title(sprintf('%.2d kg payload | %.2d kg %s | %s', m_payload, m_gas, lift_gas, balloon_name));
+plot(t, altitude, 'DisplayName', 'Altitude');
+plot(t(burst_index), altitude(burst_index), 'rx', 'DisplayName', 'Burst Event');
+yline(pressure_burst_altitude, 'k--', 'DisplayName', 'Pressure Burst Threshold');
+yline(volume_burst_altitude, 'k:', 'DisplayName', 'Volume Burst Threshold');
+xlabel('Time (s)'); ylabel('Altitude (m)');
+legend();
+subplot(3,1,2); hold on; grid on;
+plot(t, ascent_rate);
+xlabel('Time (s)'); ylabel('Ascent Rate (m/s)');
+subplot(3,1,3); hold on; grid on;
+plot(t, ascent_accel);
+xlabel('Time (s)'); ylabel('Ascent Acceleration (m/s^2)');
+
+%% Open system, balloon and payload lose mass over time
+%% Control loop with bleeding ballast/lift gas as control inputs
