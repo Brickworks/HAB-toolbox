a14c1d750c8074faec60149d5155adc8a109b8b3
philiplinden <lindenphilipj@gmail.com>
2020-07-23 23:28:52 -0700

    CHECKPOINT: going to bed. preparing for initializing balloon parameters then exploring scikit-aero

--- hab_toolbox/test_ascent.py
+++ hab_toolbox/test_ascent.py
@@ -1,4 +1,9 @@
 import logging
 
-from balloon_library import balloon
+from balloon_library import Balloon
+
+
+LOGFORMAT = '%(asctime)-15s %(levelname)+8s: %(message)s'
+logging.basicConfig(format=LOGFORMAT, datefmt="%Y-%m-%dT%H:%M:%S%z")
+log = logging.getLogger()
 
