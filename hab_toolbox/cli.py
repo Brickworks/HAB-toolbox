2d4138e7cb4ef4cc504dab150af85d7e0ef92d83
Philip Linden <lindenphilipj@gmail.com>
2020-07-30 08:40:56 -0700

    DOC: improve CLI help text

--- hab_toolbox/cli.py
+++ hab_toolbox/cli.py
@@ -15,8 +15,10 @@ log = logging.getLogger()
 
 
 @click.group()
-@click.option('-v', '--verbose', is_flag=True)
-@click.option('--debug', is_flag=True)
+@click.option('-v', '--verbose', is_flag=True, 
+    help='Show log messages at the INFO level and above.')
+@click.option('--debug', is_flag=True, 
+    help='Show log messages at the DEBUG level and above.')
 def cli(verbose, debug):
     if debug:
         log.setLevel(logging.DEBUG)
@@ -32,9 +34,37 @@ def cli(verbose, debug):
     help='Save output to file.'
 )
 @click.option('-p', '--plot', is_flag=True, 
-    help='Plot altitude after simulating.'
+    help='Plot altitude, velocity, and acceleration after simulating.'
 )
 def sim(config_file, save_output, plot):
+    ''' Start a simulation. 
+    
+    Specify initial conditions and configurable parameters with a CONFIG_FILE 
+    formatted as a JSON.
+
+    \b
+    "balloon": (required)
+        "type": Part number of the balloon to import from balloon_library
+        "reserve_mass_kg": Mass of lift gas to always keep in balloon (kg)
+        "bleed_mass_kg": Mass of lift gas allowed to be bled from balloon (kg)
+    "payload": (required)
+        "bus_mass_kg": Mass of non-ballast payload mass (kg)
+        "ballast_mass_kg": Mass of ballast material (kg)
+    "pid": (optional)
+        "mode": Altitude controller mode. [pwm, continuous]
+        "bleed_rate_kgps": Mass flow rate of lift gas bleed (kg/s)
+        "ballast_rate_kgps": Mass flow rate of ballast release (kg/s)
+        "gains":
+            "kp": Proportional gain
+            "ki": Integral gain
+            "kd": Derivative gain
+            "n":  Filter coefficient
+    "simulation": (required)
+        "duration": Max time duration of simulation (seconds)
+        "dt": Time step (seconds)
+        "initial_altitude": Altitude at simulation start (m), [-5004 to 80000]
+        "initial_velocity": Velocity at simulation start (m/s)
+    '''
     sim_config = json.load(config_file)
     t, h, v, a = simulation.run(sim_config)
     if save_output:
